011e1e0c1b3724be4c803db78038d5df255aef64
vikassahu123 <135429562+vikassahu123@users.noreply.github.com>
2025-02-23 22:47:40 +0530

    Vikas Sahu UC8 delete greeting  from repository

---
+++ Spring_apps/SpringApps/Repository/GreetingRepository.java
@@ -0,0 +1,41 @@
+
+package com.Spring_apps.SpringApps.Repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Repository
+public class GreetingRepository {
+
+    private String greetingMessage;
+    //  it is used to save the greeting
+    public void save(String message) {
+        this.greetingMessage = message;
+    }
+    //  it is used to return the greeting
+    public String find() {
+        return greetingMessage;
+    }
+
+    //........................................
+    //   this code is for uc5, save greetings with id
+    private final Map<Long, String> greetings = new HashMap<>();
+
+    public void savebyid(Long id, String message) {
+        greetings.put(id, message);
+    }
+
+    public String findById(Long id) {
+        return greetings.getOrDefault(id, "Greeting not found!");
+    }
+
+    //...............
+    // uc6
+    public Map<Long, String> getAllGreetings() {
+        return greetings; // Assuming `greetings` is your Map<Long, String>
+    }
+
+}
---
+++ Spring_apps/SpringApps/ServiceLayer/SimpleGreeting.java
@@ -0,0 +1,73 @@
+package com.Spring_apps.SpringApps.ServiceLayer;
+
+import com.Spring_apps.SpringApps.Repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class SimpleGreeting {
+
+    public static String getGreeting()
+    {
+
+        return "Hello World";
+    }
+
+    //.............................
+    //  this code is for repository
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    public void saveGreeting(String message) {
+        greetingRepository.save(message); // call same method by same class obj.
+    }
+
+    public String getGreeting2() {
+        return greetingRepository.find();  // call same method by same class obj.
+    }
+
+
+    //.................................
+    //  this code is for uc5
+    public void saveGreetingbyid(Long id, String message) {
+        greetingRepository.savebyid(id, message);
+    }
+
+    public String getGreetingById(Long id) {
+        return greetingRepository.findById(id);
+    }
+
+    //...............
+    //  uc6
+    public Map<Long, String> getAllGreetings() {
+        return greetingRepository.getAllGreetings();
+    }
+
+    //...................
+    //uc7
+    public String updateGreeting(Long id, String newMessage) {
+        Map<Long,String>greetings=greetingRepository.getAllGreetings();
+        if (greetings.containsKey(id)) {
+            String oldMessage = greetings.get(id);
+            greetings.put(id, newMessage);
+            return "Original Message: " + oldMessage + " | Updated Message: " + newMessage;
+        } else {
+            return "Greeting with ID " + id + " not found!";
+        }
+    }
+
+    //......................
+    //uc8
+    public String deleteGreetingById(Long id) {
+
+        Map<Long,String>greetings=greetingRepository.getAllGreetings();
+        if (greetings.containsKey(id)) {
+            greetings.remove(id);
+            return "Greeting with ID " + id + " has been deleted successfully!";
+        } else {
+            return "Greeting with ID " + id + " not found!";
+        }
+    }
+}
